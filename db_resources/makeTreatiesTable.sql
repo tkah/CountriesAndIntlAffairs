03186f27300024dea5cb48fb12f7e190b71aa4f1
xinyu2 <chenxinyu@unm.edu>
2016-03-08 11:25:47 -0700

    Added files via upload

---
+++ db_resources/makeTreatiesTable.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Treaties(
+  regNumber varchar(7),
+  name  varchar(255)  NOT NULL,
+  dateEnforced  varchar(8) NOT NULL,
+  description varchar(255) NOT NULL,
+  wikipage  varchar(255) NOT NULL,
+  PRIMARY KEY(regNumber)
+);
+
+INSERT INTO Treaties VALUES
+('I-35597','Convention on the Prohibition of the Use, Stockpiling, Production and Transfer of Anti-Personnel Mines and on their Destruction','19990301','Prohibition of the Use, Stockpiling, Production and Transfer of Anti-Personnel Mines and on their Destruction','https://en.wikipedia.org/wiki/List_of_parties_to_the_Ottawa_Treaty');
+
+INSERT INTO Treaties VALUES
+('I-38544','Rome Statute of the International Criminal Court','20020701','Rome Statute of the International Criminal Court','https://en.wikipedia.org/wiki/States_parties_to_the_Rome_Statute_of_the_International_Criminal_Court');
+
+INSERT INTO Treaties VALUES
+('I-20378','Convention on the Elimination of All Forms of Discrimination against Women','19810903','Elimination of All Forms of Discrimination against Women','https://en.wikipedia.org/wiki/List_of_parties_to_the_Convention_on_the_Elimination_of_All_Forms_of_Discrimination_Against_Women');
+
+INSERT INTO Treaties VALUES
+('I-30822','United Nations Framework Convention on Climate Change','19940321','United Nations Framework Convention on Climate Change','https://en.wikipedia.org/wiki/List_of_parties_to_the_United_Nations_Framework_Convention_on_Climate_Change');
+
+INSERT INTO Treaties VALUES
+('A-30822','Kyoto Protocol to the United Nations Framework Convention on Climate Change','20050216','Kyoto Protocol to the United Nations Framework Convention on Climate Change','https://en.wikipedia.org/wiki/List_of_parties_to_the_Kyoto_Protocol');
+
+INSERT INTO Treaties VALUES
+('I-1021','Convention on the Prevention and Punishment of the Crime of Genocide','19510112','Prevention and Punishment of the Crime of Genocide (CPPCG)','https://en.wikipedia.org/wiki/Genocide_Convention');
+
+INSERT INTO Treaties VALUES
+('I-31363','United Nations Convention on the Law of the Sea','19941116','United Nations Convention on the Law of the Sea','https://en.wikipedia.org/wiki/List_of_parties_to_the_United_Nations_Convention_on_the_Law_of_the_Sea');
+
+INSERT INTO Treaties VALUES
+('I-14860','Convention on the prohibition of the development, production and stockpiling of bacteriological (biological) and toxin weapons and on their destruction','19750326','Convention on the prohibition of the development, production and stockpiling of bacteriological (biological) and toxin weapons and on their destruction','https://en.wikipedia.org/wiki/List_of_parties_to_the_Biological_Weapons_Convention');
+
+INSERT INTO Treaties VALUES
+('I-33757','Convention on the Prohibition of the Development, Production, Stockpiling and Use of Chemical Weapons and on their Destruction','19970429','Convention on the Prohibition of the Development, Production, Stockpiling and Use of Chemical Weapons and on their Destruction','https://en.wikipedia.org/wiki/List_of_parties_to_the_Chemical_Weapons_Convention');
+
+INSERT INTO Treaties VALUES
+('I-10485','Treaty on the Non-Proliferation of Nuclear Weapons','19700305','Treaty on the Non-Proliferation of Nuclear Weapons','https://en.wikipedia.org/wiki/List_of_parties_to_the_Treaty_on_the_Non-Proliferation_of_Nuclear_Weapons');
+
+
+
