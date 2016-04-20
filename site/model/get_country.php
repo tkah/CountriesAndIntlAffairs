384cb0739f6bf63f9bc934e0e594ecc620c0e39c
c <chenxinyu@unm.edu>
2016-04-20 01:37:53 -0600

    query top10 immgrants

--- site/model/get_country.php
+++ site/model/get_country.php
@@ -32,4 +32,21 @@ $query->execute();
 $languages = $query->fetchAll(PDO::FETCH_ASSOC);
 $country['languages'] = $languages;
 
+/* Get Migrations table data and assign array to country object */
+$query = $connection->prepare("
+SELECT c.name as origCtry, m.totalAmount as ttlAmt
+FROM Countries c, Migrations m
+WHERE m.destCountry = :c_num
+AND m.origCountry = c.countryNumber
+AND inYear = (select max(inYear) from Migrations where destCountry= :c_num2)
+ORDER BY m.totalAmount desc
+LIMIT 10;
+");
+
+$query->bindValue(':c_num', $country['countryNumber'], PDO::PARAM_STR);
+$query->bindValue(':c_num2', $country['countryNumber'], PDO::PARAM_STR);
+$query->execute();
+$immigrants = $query->fetchAll(PDO::FETCH_ASSOC);
+$country['immigrants'] = $immigrants;
+
 echo json_encode($country);
