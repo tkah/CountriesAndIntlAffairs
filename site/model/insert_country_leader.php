f0e8627a1c61947a30d39122afad1390ef054a1c
c <chenxinyu@unm.edu>
2016-05-03 01:07:22 -0600

    add transaction to insert leader

--- site/model/insert_country_leader.php
+++ site/model/insert_country_leader.php
@@ -7,21 +7,27 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 $leader = $request->leader;
 $country = $request->country;
+try{
+    $connection->beginTransaction();
+    $query = $connection->prepare("
+        INSERT INTO Leaders
+        VALUES (:name, :type, :termStart, :termEnd);
+    ");
+    $query->bindValue(':name', $leader->name, PDO::PARAM_STR);
+    $query->bindValue(':type', $leader->type, PDO::PARAM_STR);
+    $query->bindValue(':termStart', $leader->termStart, PDO::PARAM_STR);
+    $query->bindValue(':termEnd', $leader->termEnd, PDO::PARAM_STR);
+    $query->execute();
+    
+    $query = $connection->prepare("
+        INSERT INTO LeaderOf
+        VALUES (:c_code, :name);
+    ");
+    $query->bindValue(':c_code', $country, PDO::PARAM_STR);
+    $query->bindValue(':name', $leader->name, PDO::PARAM_STR);
+    $query->execute();    
+    $connection->commit();
+}catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+}
 
-$query = $connection->prepare("
-    INSERT INTO Leaders
-    VALUES (:name, :type, :termStart, :termEnd)
-");
-$query->bindValue(':name', $leader->name, PDO::PARAM_STR);
-$query->bindValue(':type', $leader->type, PDO::PARAM_STR);
-$query->bindValue(':termStart', $leader->termStart, PDO::PARAM_STR);
-$query->bindValue(':termEnd', $leader->termEnd, PDO::PARAM_STR);
-$query->execute();
-
-$query = $connection->prepare("
-    INSERT INTO LeaderOf
-    VALUES (:c_code, :name)
-");
-$query->bindValue(':c_code', $country, PDO::PARAM_STR);
-$query->bindValue(':name', $leader->name, PDO::PARAM_STR);
-$query->execute();
