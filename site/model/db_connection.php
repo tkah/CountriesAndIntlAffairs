94287e5015cc21020cb98dcdf551e41569b76da7
TorranK <tkahleck@unm.edu>
2016-08-24 21:56:49 -0600

    added part 4 power point presentation

--- site/model/db_connection.php
+++ site/model/db_connection.php
@@ -1,11 +1,12 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "root";
+$username = "torran7";
+$password = "answer42!";
+$dbname = "torran7_countriesAndIntlAffairs";
 
 try {
-    $connection = new PDO("mysql:host=$servername;dbname=countriesAndIntlAffairs;charset=utf8", $username, $password);
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     // set the PDO error mode to exception
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
